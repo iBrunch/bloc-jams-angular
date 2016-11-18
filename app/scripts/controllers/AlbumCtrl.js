105cce375208a59cee1fe2a8f9724b5143926a23
Scott Arakawa <randallarakawa@Randalls-MacBook-Pro.local>
2016-11-18 01:33:00 -0600

    Assignment 6 Services Part 1 - Complete

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -2,8 +2,8 @@
 	function AlbumCtrl(Fixtures) {
 		this.albumData = Fixtures.getAlbum();
 		this.songData = [];
-		for (var i=0; i < albumPicasso.songs.length; i++) {
-			this.songData.push(angular.copy(albumPicasso.songs[i]));
+		for (var i=0; i < this.albumData.songs.length; i++) {
+			this.songData.push(angular.copy(this.albumData.songs[i]));
 		}
 	}
 
