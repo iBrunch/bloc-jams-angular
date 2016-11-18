96cfcda93444d25fc41f7d24b8dcf88cd9263c68
Scott Arakawa <randallarakawa@Randalls-MacBook-Pro.local>
2016-11-18 00:46:46 -0600

    Checkpoint 6 Services Part 1

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,6 +1,6 @@
 (function() {
-	function AlbumCtrl() {
-		this.albumData = albumPicasso;
+	function AlbumCtrl(Fixtures) {
+		this.albumData = Fixtures.getAlbum();
 		this.songData = [];
 		for (var i=0; i < albumPicasso.songs.length; i++) {
 			this.songData.push(angular.copy(albumPicasso.songs[i]));
