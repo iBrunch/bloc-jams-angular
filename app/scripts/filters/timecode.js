77e235d87e4f142531882ce87e8e23929a66f023
Scott Arakawa <randallarakawa@Randalls-MacBook-Pro.local>
2016-12-01 02:42:26 -0600

    Assignment 11 Filters - Complete

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,25 +1,13 @@
 (function() {
     function timecode() {
         return function(seconds) {
-			var seconds = Number.parseFloat(seconds);
+			var timer = buzz.toTimer(seconds);
 
 		    if (Number.isNaN(seconds)) {
 		        return '-:--';
 		    }
 
-			var wholeSeconds = Math.floor(seconds);
-			var minutes = Math.floor(wholeSeconds / 60);
-			var remainingSeconds = wholeSeconds % 60;
-
-			var output = minutes + ':';
-
-			if (remainingSeconds < 10) {
-				output += '0';   
-			}
-
-			output += remainingSeconds;
-
-			return output;
+			return timer;
         };
     }
  
